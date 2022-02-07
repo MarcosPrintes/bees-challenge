6bbca2a45c3feace92371e742f64855d736d9998
Marcos Printes <marcos.printes@dotgroup.com.br>
2022-02-07 08:35:03 -0300

    feat: add breweries page [WIP]

---
+++ src/pages/Breweries/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from '@/components/Header';
+import { Container, Content } from './styles';
+export const Breweries = () => {
+  return (
+    <Container>
+      <Header />
+      <Content></Content>
+    </Container>
+  );
+};
---
+++ src/pages/Breweries/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+
+export const Content = styled.div`
+  flex: 1;
+  background: #fffef0;
+`;
